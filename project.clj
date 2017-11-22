a01ba4e4eafb9464bb57aec21471f9cdbf53af26
Andrey Antukh <niwi@niwi.nz>
2017-11-22 16:48:00 +0100

    Reorganize the source tree.
    The clj/cljs separation is no longer needed having
    conditional readers if we need support cljs in the
    near future.

--- project.clj
+++ project.clj
@@ -1,15 +1,21 @@
 (defproject recycle "0.1.0-SNAPSHOT"
   :description "Clojure library designed to manage application state heavily inspired by stuartsierra's component."
-  :url "https://github.com/anler/recycle"
+  :url "https://github.com/funcool/recycle"
   :license {:name "BSD (2-Clause)"
             :url  "http://opensource.org/licenses/BSD-2-Clause"}
+
+  :jar-exclusions [#"\.swp|\.swo|user.clj"]
+
   :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
-                 [anler/try "0.3.0"]
                  [org.clojure/core.async "0.3.443"]
                  [org.clojure/tools.logging "0.4.0"]]
-  :source-paths ["src/clj" "src/cljc" "src/cljs"]
-  :test-paths ["test/clj" "test/cljc" "test/cljs"]
-  :plugins [[funcool/codeina "0.4.0"
-             :exclusions [org.clojure/clojure]]]
+
+  :source-paths ["src"]
+  :test-paths ["test"]
+
   :codeina {:sources ["src/clj"]
-            :reader  :clojure})
+            :reader  :clojure}
+
+  :profiles
+  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
+         :plugins [[funcool/codeina "0.5.0" :exclusions [org.clojure/clojure]]]}})
